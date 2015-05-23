72e78ccee510288d1c64fe7aad4b78bbaeeab0a1
Soufiane BAROUDI <soufiane.barodi@gmail.com>
2015-05-23 22:26:46 +0200

    Bug Fixed / Add Ajax ( OK )/ Data on Map ( N_OK )
    Uncaught InvalidValueError : not a Feature or FeatureCollection !!

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var gdal = require('./routes/gdal');
-
 var app = express();
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,8 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/users', gdal);
-
+app.use('/gdal', gdal);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -57,4 +57,6 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+
 module.exports = app;
--- routes/gdal.js
+++ routes/gdal.js
@@ -5,10 +5,11 @@ var router = express.Router();
 
 var dataset = gdal.open("../Data/parcellaire.shp");
 var layer = dataset.layers.get(0);
-var FeautureCollection = [{}];
+var FeautureCollection = new Object();
 var Features = new Array();
 
 layer.features.forEach(function(feature) {
+
 
     Features.push({ type : "feauture" , proproties : feature.clone().fields.toJSON() , geometry : feature.getGeometry().toJSON() })
 
@@ -18,8 +19,9 @@ FeautureCollection = ({ type : "FeautureCollection" , features : Features });
 
 
 router.get('/', function(req, res, next) {
-    console.log(FeautureCollection);
-    res.send(FeautureCollection);
+    //console.log(fa);
+    res.json(FeautureCollection);
 });
+
 
 module.exports = router;
