5bfc11407f86f1e252740a7c101425bb7ccaa918
Soufiane BAROUDI <soufiane.barodi@gmail.com>
2015-05-25 12:16:20 +0200

    Add Data to map ( Ok )

--- routes/gdal.js
+++ routes/gdal.js
@@ -1,41 +1,29 @@
 var express = require('express');
 var gdal = require('gdal');
-var fs = require('fs');
 var router = express.Router();
 
 
 var dataset = gdal.open("../Data/parcellaire.shp");
 var layer = dataset.layers.get(0);
-var FeautureCollection = new Object();
-var Features = new Array();
+var FeautureCollection = {
+    type : "FeatureCollection" ,
+    features : [] };
 
 
 
-fs.readFile('../Data/file.json', 'utf8', function (err,data) {
- if (err) {
- return console.log(err);
- }
-
- FeautureCollection = data ;
+layer.features.forEach(function(feature) {
 
 
- // console.log(FeautureCollection);
-
- });
-
-/*layer.features.forEach(function(feature) {
-
-
-    Features.push({ type : "feauture" , proproties : feature.clone().fields.toJSON() , geometry : feature.getGeometry().toJSON() })
+    FeautureCollection.features.push({ "type" : "Feature" ,
+        properties : JSON.parse(feature.clone().fields.toJSON()) ,
+        geometry : JSON.parse(feature.getGeometry().toJSON())})
 
 });
-
-FeautureCollection = ({ type : "FeautureCollection" , features : Features });*/
-
 
 router.get('/', function(req, res, next) {
 
     res.send(FeautureCollection);
+
 });
 
 
